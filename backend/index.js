cd4552af04aecee2084c7d7f82e42132574769f8
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-06-15 17:14:10 +0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+// Bize veri gönderecek masa hakemleri
+let socket = require('socket.io-client')('http://localhost:5555');
+
+const express = require("express");
+const app = express();
+const appServer = require("http").createServer(app);
+const channel = require("socket.io")(appServer,{
+	cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+const PORT = 8080;
+
+// örnek simülasyon verimiz. Hız, devir ve motor sıcaklığı gibi
+let droneData = {
+	"lat" : 40.9126844,
+	"lon" : 29.10498
+}
+
+let engineData = {
+	"speed": 0,
+	"rpm": 0,
+	"heat": 0
+};
+
+channel.on('connection', mysocket => {
+
+	// Beni kanser eden kod, bazen çalışıyor bazen çalışmıyordu
+	// Ancak önyüzü hakem serverlarına bağladığımda sorun çözüldü
+	socket.on("output road", data => {
+		console.log("#############");
+		console.log(data);
+		console.log("#############");
+		mysocket.broadcast.emit("frontend data", data);	
+	})
+
+})
+
+appServer.listen(PORT, () => {
+	console.log(`${Date(Date.now()).toLocaleString()}: Sunucu ${PORT} nolu port üzerinden aktif konumda.`);
+})
+
+// Her 5 saniyede bir çalışacak bir fonksiyon.
+setInterval(function () {
+	// Rastgele veriler üretiyoruz.
+	engineData.speed = getRandomValue(70, 180);
+	engineData.rpm = getRandomValue(1000, 10000);
+	engineData.heat = getRandomValue(100, 500);
+
+	droneData.lat = getRandomValue(40.25,40.26);
+	droneData.lon = getRandomValue(29.55,29.56);
+
+	// Veriyi emit metodu ile "input road" namespace'ini kullanarak sunucuya yolluyoruz
+	// oradaki callback'de devreye girip bu veriyi bağlı olan diğer istemcilere 
+	// (output road, namespace'ini kullanan) yayınlayacak.
+	socket.emit("input road", droneData);
+}, 2000);
+
+
+function getRandomValue(min, max)
+{
+	return Number((Math.random() * (min - max) + max).toFixed(4))
+}
---
+++ hakem/index.js
@@ -0,0 +1,66 @@
+
+
+const express = require("express");
+const app = express();
+const appServer = require("http").createServer(app);
+const channel = require("socket.io")(appServer,{
+	cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+const PORT = 5555;
+
+
+channel.on('connection', socket => {
+
+	console.log(`${Date(Date.now()).toLocaleString()}: yeni bir istemci bağlandı`);
+
+	socket.on('input road', (data) => {
+		
+		console.log("gelen veriler : ");
+		console.log(data);
+
+		channel.emit("output road", [
+			{
+				"lat" : getRandomValue(40.25,40.26),
+				"lon" : getRandomValue(29.55,29.56),
+				"height" : getRandomValue(10,100)
+			},
+			{
+				"lat" : getRandomValue(40.25,40.26),
+				"lon" : getRandomValue(29.55,29.56),
+				"height" : getRandomValue(10,100)
+			},
+			{
+				"lat" : getRandomValue(40.25,40.26),
+				"lon" : getRandomValue(29.55,29.56),
+				"height" : getRandomValue(10,100)
+			}
+		]
+		);  // burası callback metodumuz olarak düşünülebilir
+
+	})
+
+
+	socket.on('disconnect', () => {
+
+		console.log(`${Date(Date.now()).toLocaleString()}istemci bağlantıyı kapattı`);
+
+	})
+
+
+
+})
+
+appServer.listen(PORT, () => {
+
+	console.log(`${Date(Date.now()).toLocaleString()}: Sunucu ${PORT} nolu port üzerinden aktif konumda.`);
+
+})
+
+function getRandomValue(min, max)
+{
+	return Number((Math.random() * (min - max) + max).toFixed(4))
+}
