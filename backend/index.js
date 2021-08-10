315c9ebd7236283a0e825591f0d7c6702b9efefb
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-08-10 18:14:29 +0300

    web soketi kaldırıldı

--- backend/index.js
+++ backend/index.js
@@ -4,26 +4,16 @@ let socket = require('socket.io-client')('http://localhost:5555');
 const express = require("express");
 const app = express();
 const appServer = require("http").createServer(app);
-const channel = require("socket.io")(appServer,{
-	cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
 
 const PORT = 8080;
 
-// örnek simülasyon verimiz. Hız, devir ve motor sıcaklığı gibi
+// örnek simülasyon verimiz
 let droneData = {
 	"lat" : 40.9126844,
-	"lon" : 29.10498
+	"lon" : 29.10498,
+	"height" : 50,
+	"mode" : false
 }
-
-let engineData = {
-	"speed": 0,
-	"rpm": 0,
-	"heat": 0
-};
 
 appServer.listen(PORT, () => {
 	console.log(`${Date(Date.now()).toLocaleString()}: Sunucu ${PORT} nolu port üzerinden aktif konumda.`);
@@ -32,28 +22,16 @@ appServer.listen(PORT, () => {
 // Her 5 saniyede bir çalışacak bir fonksiyon.
 setInterval(function () {
 	// Rastgele veriler üretiyoruz.
-	engineData.speed = getRandomValue(70, 180);
-	engineData.rpm = getRandomValue(1000, 10000);
-	engineData.heat = getRandomValue(100, 500);
 
 	droneData.lat = getRandomValue(40.25,40.26);
 	droneData.lon = getRandomValue(29.55,29.56);
+	droneData.height = getRandomValue(10,100);
+	droneData.mode = getRandomValue(0,1) > 0.5 ? true : false;
 
-	// Veriyi emit metodu ile "input road" namespace'ini kullanarak sunucuya yolluyoruz
-	// oradaki callback'de devreye girip bu veriyi bağlı olan diğer istemcilere 
-	// (output road, namespace'ini kullanan) yayınlayacak.
+	// Oluşturduğumuz rastgele verileri hakem sunucusuna yolluyoruz
 	socket.emit("input road", droneData);
 }, 2000);
 
-
-channel.on('connection', mysocket => {
-	console.log('front end bağlandı');
-
-	socket.on('output road', data => {
-		console.log("all drone datas : ", data);
-		mysocket.emit("frontend data", data);
-	})
-})
 
 function getRandomValue(min, max)
 {
